1c1b1491444aa16782f1002dcb940e77d2318338
Heng Chhayhong <Henqchhayhong275@gmail.com>
2023-07-03 22:12:26 +0700

    TP13

---
+++ web/src/components/logoHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>លេខមួយ.com</h1>
+    </div>
+</template>
+
+<script setup>
+    import { RouterLink } from 'vue-router';
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css2?family=Ysabeau+SC:wght@300&display=swap');
+    h1{
+        color: white;
+    }
+    div{
+        z-index:99;
+        font-family: 'Ysabeau SC', sans-serif;
+        width: 100%;
+        text-align: center;
+        padding: 1rem 0;
+        background-color: rgb(108, 106, 106);
+        position: sticky;
+        top:0;
+        left: 0;
+        right: 0;
+    }
+</style>
